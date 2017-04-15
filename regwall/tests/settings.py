b6a04763c812248f8d319a1f2bd3f83c2030ddcf
Richard Cornish <richardcornish@gmail.com>
2017-04-15 18:11:54 -0500

    Used MIDDLEWARE_CLASSES for compatibility

--- regwall/tests/settings.py
+++ regwall/tests/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     'regwall.tests.articles',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
