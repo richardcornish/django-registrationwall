29933fd764db5b0d874d69f68e38381a88b7ffb1
Richard Cornish <richardcornish@gmail.com>
2017-04-15 18:20:57 -0500

    Updated tests for fixtures

--- regwall/tests/__init__.py
+++ regwall/tests/__init__.py
@@ -20,9 +20,9 @@ class RegWallTestCase(TestCase):
     def test_regwall(self):
         for article in Article.objects.all()[:10]:
             response = self.client.get(reverse('article_detail', args=[article.slug]))
-        self.assertEqual(response.request['PATH_INFO'], '/articles/2/')
+        self.assertEqual(response.request['PATH_INFO'], '/articles/study-finds-majority-accidental-heroin-overdoses-could-be-prevented-less-heroin/')
 
     def test_regwall_raise(self):
         for article in Article.objects.all():
             response = self.client.get(reverse('article_detail', args=[article.slug]))
-        self.assertRedirects(response, '/accounts/login/?next=/articles/1/')
+        self.assertRedirects(response, '/accounts/login/?next=/articles/guy-wearing-thumb-drive-around-neck-wonders-if-you-tried-hard-reboot/')
