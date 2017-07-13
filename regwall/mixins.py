bb99ed5474d00907935d48d889fa94086309b9f4
Richard Cornish <richardcornish@gmail.com>
2017-07-13 12:05:38 -0500

    Refactored mixin

--- regwall/mixins.py
+++ regwall/mixins.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 
 try:
-    from urlparse import urlparse  # Python 2
-except ImportError:
     from urllib.parse import urlparse
+except ImportError:  # Python 2
+    from urlparse import urlparse
 
 try:
     from django.contrib.auth.mixins import AccessMixin
@@ -16,27 +16,19 @@ from . import settings
 class RaiseRegWallMixin(AccessMixin):
     """View mixin that increments an anonymous user's article count."""
 
-    def get_expire_seconds(self):
-        return 60 * 60 * 24 * settings.REGWALL_EXPIRE
-
-    def get_social_list(self):
-        social_list = list(settings.REGWALL_SOCIAL)
-        social_list.extend(['www.' + social for social in social_list])
-        return social_list
-
     def get_or_create_regwall_list(self, list_name):
         try:
             regwall = self.request.session['regwall']
         except KeyError:
-            seconds = self.get_expire_seconds()
+            seconds = 60 * 60 * 24 * settings.REGWALL_EXPIRE
             self.request.session.set_expiry(seconds)
             self.request.session['regwall'] = {}
-            self.request.session.modified = True
             regwall = self.request.session['regwall']
         try:
             return regwall[list_name]
         except KeyError:
             regwall[list_name] = []
+            self.request.session.modified = True
             return regwall[list_name]
 
     def increment_regwall_list(self, regwall_list):
@@ -49,31 +41,38 @@ class RaiseRegWallMixin(AccessMixin):
         })
         self.request.session.modified = True
 
+    def is_under_limit(self, regwall_list):
+        return len(regwall_list) <= settings.REGWALL_LIMIT
+
+    @property
     def is_authenticated(self):
         try:
             return self.request.user.is_authenticated()
         except TypeError:
             return self.request.user.is_authenticated
 
+    @property
     def is_social(self):
+        social_list = list(settings.REGWALL_SOCIAL)
+        social_list.extend(['www.' + social for social in social_list])
         try:
-            url_tuple = urlparse(self.request.META['HTTP_REFERER'])
+            referer = self.request.META['HTTP_REFERER']
         except KeyError:
             return False
-        return url_tuple.netloc in self.get_social_list()
+        url_tuple = urlparse(referer)
+        return url_tuple.netloc in social_list
 
-    def is_under_limit(self, regwall_list):
-        return len(regwall_list) <= settings.REGWALL_LIMIT
-
-    def has_visited(self, successes_list):
-        obj = self.get_object()
+    @property
+    def has_visited(self):
+        successes_list = self.get_or_create_regwall_list('successes')
         value_list = [article[key] for article in successes_list for key in article]
+        obj = self.get_object()
         return obj.get_absolute_url() in value_list
 
     def dispatch(self, request, *args, **kwargs):
         attempts_list = self.get_or_create_regwall_list('attempts')
         successes_list = self.get_or_create_regwall_list('successes')
-        if not self.is_authenticated() and not self.is_social() and not self.has_visited(successes_list):
+        if not self.is_authenticated and not self.is_social and not self.has_visited:
             self.increment_regwall_list(attempts_list)
             if self.is_under_limit(attempts_list):
                 self.increment_regwall_list(successes_list)
